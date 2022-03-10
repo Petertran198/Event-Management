c0b6188e90b41b7c392daa2d95e6b0869a80b8fb
petertran98 <Petertran198@gmail.com>
2022-03-10 15:34:06 -0800

    Added comments about @NgModule and see how to import components, pipes, directives, & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [EventAppComponent], // <- When you want to add a component, pipe, or directive u must declare them here
+  imports: [BrowserModule], // <-- Used to import other modules
+  providers: [], // <-- To add services here
+  bootstrap: [EventAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app-root',
+  templateUrl: './events-app.component.html',
+  styleUrls: ['./events-app.component.css'],
+})
+export class EventAppComponent {
+  title = 'Event Management';
+}
