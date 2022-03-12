d90c127e297b73e5240c5dab14025ac581565540
petertran98 <Petertran198@gmail.com>
2022-03-11 16:20:50 -0800

    Show how to use ngSwitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,16 +6,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h1>{{ event?.name }}</h1>
       <div>Date: {{ event?.name }}</div>
-      <div>Time: {{ event?.time }}</div>
+      <div [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{ event?.price }}</div>
-      <!-- The safe navigation operator will check for null, allowing the field to display if it isnt empty -->
-      <!-- It will shortcircut everything after so no need to continiously put ? after ever properties -->
-      <!-- but for example if u want the second property location to be nullable and will only display if there is one 
-          you must chain it -->
-      <!-- In this example it is saying if there is an event and if there is a location then show address  -->
-      <div>Location: {{ event?.location?.address }}</div>
-      <!-- Meanwhile this example it is saying there is an event and if the event has a  location then show address  -->
-      <div>Location: {{ event.location?.address }}</div>
+      <div [hidden]="!event?.location">
+        Location: {{ event?.location?.address }}
+      </div>
+      <div [hidden]="!event?.locationUrl">
+        Online Url: {{ event.locationUrl }}
+      </div>
     </div>
   </div> `,
 })
