85d376dfb1010f0e32eeab98b430eb0ad9dc126b
petertran98 <Petertran198@gmail.com>
2022-03-14 11:47:15 -0700

    Added navigation via clicking on links

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailComponent } from './events/event-details/event-details.component';
 import { EventListsComponent } from './events/event-lists.component';
 
 export const appRoutes: Routes = [
+  { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventListsComponent },
   { path: 'events/:id', component: EventDetailComponent },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
