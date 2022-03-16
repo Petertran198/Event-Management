b536184985126b97735e7ea2755fda694991156b
petertran98 <Petertran198@gmail.com>
2022-03-16 11:43:08 -0700

    Added validations to Profile  ReactiveForm

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 @Component({
   templateUrl: './profile.component.html',
   selector: 'profile',
+  styles: [
+    `
+      em {
+        float: right;
+        color: red;
+      }
+      ,
+      .error input {
+        background-color: red;
+      }
+    `,
+  ],
 })
 export class ProfileComponent implements OnInit {
   profileFormGroup: FormGroup;
-
+  firstName: FormControl;
+  lastName: FormControl;
   constructor(private authService: AuthService, private router: Router) {}
   ngOnInit(): void {
     //This form will have two inputs firstName and lastName
     //With the currentUser.firstName and lastName already inside
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
+    //Adding Validator to make it required field
+    this.firstName = new FormControl(
+      this.authService.currentUser.firstName,
+      Validators.required
+    );
+    this.lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
     //Adding these controls to the form by creating a FormGroup
     //This FormGroup has a firstName and lastName property and these properties VALUES
     //are set to the firstName and LastName FormControl we set above
     this.profileFormGroup = new FormGroup({
-      firstName: firstName,
-      lastName: lastName,
+      firstName: this.firstName,
+      lastName: this.lastName,
     });
   }
 
@@ -29,11 +49,25 @@ export class ProfileComponent implements OnInit {
       form.controls.firstName.value,
       form.controls.lastName.value
     );
-
     this.router.navigate(['./events']);
   }
 
   onCancel() {
     this.router.navigate(['./events']);
+  }
+
+  isFirstNameInValid() {
+    return (
+      //If the field is invalid
+      //Or if the field is invalid and it has been touched so this will only display after field has been clicked or 'touched'
+      this.firstName.invalid ||
+      (this.firstName.invalid && this.firstName.touched)
+    );
+  }
+
+  isLastNameInValid() {
+    return (
+      this.lastName.invalid || (this.firstName.invalid && this.lastName.touched)
+    );
   }
 }
