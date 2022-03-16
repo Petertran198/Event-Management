3f16cbb1ce2723c44b1649a7a22638b1a094118d
petertran98 <Petertran198@gmail.com>
2022-03-15 17:39:43 -0700

    Added validation to form using  #loginForm template selector and #loginForm states I.E touched isValid, invalid, etc

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,13 +4,21 @@ import { AuthService } from './auth.service';
 @Component({
   templateUrl: './login.component.html',
   selector: 'login',
+  styles: [
+    `
+      em {
+        float: right;
+        color: red;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName: string;
   password: string;
   constructor(private authService: AuthService, private router: Router) {}
+
   login(form) {
-    this.authService.login(form.userName, form.password);
-    this.router.navigate(['/events']);
+    console.log(form.controls);
   }
 }
