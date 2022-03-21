dc09a61d3674ecdfcc5a2ad9339b1e6f9d893f5f
petertran98 <Petertran198@gmail.com>
2022-03-21 15:54:13 -0700

    Added way to filter session data

--- src/app/events/event-details/session-details.component.ts
+++ src/app/events/event-details/session-details.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 
 @Component({
   selector: 'session-detail',
   templateUrl: './session-details.component.html',
 })
-export class SessionDetailsComponent {
+export class SessionDetailsComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filteredBy: string;
+  filteredSessions: ISession[];
+  //ngOnChanges runs/reruns when an @Input changes
+  ngOnChanges(): void {
+    //This allows use to sort the data in an immutabile way like react
+    if (this.filteredBy === 'all') {
+      this.filteredSessions = [...this.sessions];
+    } else {
+      this.filteredSessions = this.sessions.filter(
+        (session) => session.level === this.filteredBy
+      );
+    }
+  }
 }
